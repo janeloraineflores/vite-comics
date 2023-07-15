e07baef23bf355fd77d50a6a6edf4099bde0f184
janeloraineflores <janeloraineaflores@yahoo.com>
2023-07-15 10:45:46 +0200

    Aggiunta header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,125 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+    name: "App",
+    components: {
+    },
+    data() {
+      return {};
+    },
+    methods: {},
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    
+  <header>
+    <div class="container">
+      <div class="header-container">
+        <div class="logo-container">
+        <img src="./assets/img/dc-logo.png" alt="">
+        </div>
+
+        <ul>
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li>
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    MAIN
+  </main>
+
+  <footer>
+    FOOTER
+  </footer>
+
+  
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+  *{
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;   
+
+    &:not(ul) {
+      padding: 0;
+    }
+  }
+
+  .container {
+    margin: 0 auto;
+    width: 1200px;
+  }
+  
+  header {
+    padding: 20px 0;
+
+    .header-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .logo-container {
+      width: 80px;
+      height: 80px;
+
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    ul {
+      display: flex;
+      list-style: none;
+
+      li {
+        a{
+          display: inline-block;
+          padding: 15px;
+          color: black;
+          text-decoration: none;
+          font-size: .9375rem;
+          transition: .2s all ease-in-out;
+
+          &:hover {
+            background-color: #0282F9
+          }
+        }
+      }
+    }
+  }
 </style>
